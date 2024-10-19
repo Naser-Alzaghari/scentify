108c192ee607bb226cdf3dccb479ad4ed3f7aae9
Naser-Alzaghari <naseralzaghari88@gmail.com>
2024-10-19 18:10:47 +0300

    update contact us

--- public/contact_us.php
+++ public/contact_us.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en-US" dir="ltr">
-
+<?php session_start(); ?>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -47,7 +47,7 @@
                             <h1 class="fs-4 fs-lg-8 fs-md-6 fw-bold mb-5">Contact us</h1>
                         </div>
                         <div class="bg-300 card p-3 border border-3 border-dark">
-                            <form id="contact_us">
+                            <form id="contact_us" method="POST" action="send_email.php">
                                 <div class="row">
                                     <div class="mb-3 col-md-4">
                                         <label for="full_name" class="form-label">Full Name</label>
@@ -73,9 +73,10 @@
                                 
                                 <div class="text-center">
                                     <button type="submit" class="btn btn-primary">Send</button>
-
                                 </div>
                             </form>
+                            <p class="text-success d-none" id="message_sent">message sent successfully</p>
+                            <p class="text-danger d-none" id="message_fail">message faild to send</p>
                         </div>
                     </div>
                 </div>
@@ -84,59 +85,20 @@
     </main>
 
     <?php include "footer.html" ?>
-
+    <?php
+        if(isset($_SESSION["mail"])){
+            if($_SESSION["mail"]){
+                echo "<script>document.getElementById('message_sent').classList.remove('d-none')</script>";
+            } else {
+                echo "<script>document.getElementById('message_fail').classList.remove('d-none')</script>";
+            }
+            unset($_SESSION["mail"]);
+        }
+    ?>
     <script src="assets/js/theme.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://smtpjs.com/v3/smtp.js"></script>
-    <script>
-    const form = document.getElementById('contact_us');    
-    const fullName = document.getElementById("full_name");
-    const email = document.getElementById("email_address");
-    const subject = document.getElementById("subject");
-    const Message = document.getElementById("Message");
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        sendEmail();
-    });
-
-    function sendEmail() {
-        const data = {
-            fullName: fullName.value,
-            email: email.value,
-            subject: subject.value,
-            message: Message.value
-        };
-
-        const jsonData = JSON.stringify(data);
-        console.log(jsonData);
-
-        const bodyMessage = `
-                <strong>Full Name:</strong> ${fullName.value}<br>
-                <strong>Email:</strong> ${email.value}<br>
-                <strong>Subject:</strong> ${subject.value}<br>
-                <strong>Message:</strong> ${Message.value}<br>
-                <strong>jsonData:</strong> ${jsonData}
-            `;
-
-        Email.send({
-            Host: "smtp.elasticemail.com",
-            Username: "naseralzaghari@hotmail.com",
-            Password: "56C2E389D8DB693CA5B0F8DA6E6740A366D3",
-            To: 'naseralzaghari@hotmail.com',
-            From: "naseralzaghari@hotmail.com",
-            Subject: subject.value,
-            Body: bodyMessage
-        }).then(
-            message => Swal.fire({
-                title: "Success!",
-                text: "Message sent successfully!",
-                icon: "success"
-            })
-        ).catch(
-            error => alert("Failed to send message")
-        );
-    }
-    </script>
+    
     </script>
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@200;300;400;500;600;700;800;900&amp;display=swap"
         rel="stylesheet">
---
+++ public/send_email.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+require "../vendor/phpmailer/phpmailer/src/Exception.php";
+require "../vendor/phpmailer/phpmailer/src/PHPMailer.php";
+require "../vendor/phpmailer/phpmailer/src/SMTP.php";
+
+$name = $_POST["full_name"];
+$email = $_POST["email_address"];
+$subject = $_POST["subject"];
+$message = $_POST["Message"];
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPAuth = true;
+$mail->Username = "naseralzaghari90@gmail.com";
+$mail->Password = "xaxhebprakhgavhq";
+$mail->SMTPSecure = "ssl";
+$mail->Port = 465;
+
+$mail->setFrom("naseralzaghari90@gmail.com");
+$mail->addAddress("naseralzaghari90@gmail.com");
+$mail->isHTML = true;
+$mail->Subject = "Scentify: $subject";
+$mail->Body = "Name: $name\r\n
+Email: $email\r\n
+Message: $message\r\n
+This message was sent from your website Scentify.";
+
+try {
+    $mail->send();
+    $_SESSION["mail"] = true;
+} catch (e){
+    $_SESSION["mail"] = false;
+}
+
+
+header("Location: contact_us.php");
+?>
