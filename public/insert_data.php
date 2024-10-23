806dbbcc60e38d31f11ef1bc59d2370e08b8f719
Naser-Alzaghari <naseralzaghari88@gmail.com>
2024-10-23 13:47:39 +0300

    Update

--- public/insert_data.php
+++ public/insert_data.php
@@ -7,11 +7,30 @@
 </head>
 <body>
     <?php
-    echo "product_id: {$_POST['add_item_id']}";
+    $product_id = $_POST['add_item_id'];
+    $quantity = $_POST['quantity'];
+    echo "product_id: $product_id";
     echo "<br>";
-    echo "quantity: {$_POST['quantity']}";
+    echo "quantity: $quantity";
     include "conn.php";
+    $query="SELECT * FROM `products` WHERE product_id = :product_id";
+    $statment=$conn->prepare($query);
+    $statment->bindParam(':product_id',$product_id,PDO::PARAM_INT);
+    $statment->execute();
+    $product = $statment->fetch(PDO::FETCH_ASSOC);
+    echo "<br><br><br>";
+    print_r($product);
     
+
+    $sql = "INSERT INTO `order_items`(`order_id`, `product_id`, `quantity`, `price`, `on_cart`) VALUES (:order_id, :product_id, :quantity, :price, :on_cart)";
+    $statment = $conn->prepare($sql);
+    $data=[
+        // 'order_id' => $name,
+        'product_id' => $product_id,
+        'quantity' => $quantity, 
+        'price' => $product['price'], 
+        'on_cart' => true, 
+    ];
     ?>
 </body>
 </html>
