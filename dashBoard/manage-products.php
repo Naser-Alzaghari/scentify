2274048858db28fcb54b07d2f0e28448bfcbbffb
Razanhroub <razan.b.alhroub@gmail.com>
2024-10-31 12:28:33 +0300

    Razan

--- dashBoard/manage-products.php
+++ dashBoard/manage-products.php
@@ -296,10 +296,9 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
                 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
                 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js">
-                < script src = "./Search.js" >
-                </script>
-                </script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"> </script>
+                <script src = "./Search.js" > </script>
+               
                 <script>
                 function clearForm() {
                     document.getElementById('productForm').reset();
